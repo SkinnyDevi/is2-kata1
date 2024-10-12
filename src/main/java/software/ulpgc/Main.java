6914477b25493616b2d608590bbdc7581bf7f749
Félix M.V <71847703+SkinnyDevi@users.noreply.github.com>
2024-10-12 20:28:01 +0100

    initial commit and creation of Person class

---
+++ src/main/java/software/ulpgc/Main.java
@@ -0,0 +1,14 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Person johnDoe = new Person(
+                "John Doe",
+                LocalDate.of(2002, 3, 19),
+                987654321
+        );
+        System.out.println(johnDoe);
+    }
+}
---
+++ src/main/java/software/ulpgc/Person.java
@@ -0,0 +1,45 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Person {
+    public static final int DaysPerYear = 365;
+    private final String name;
+    private final LocalDate birthday;
+    private final int phoneNumber;
+
+    public Person(String name, LocalDate birthday, int phoneNumber) {
+        this.name = name;
+        this.birthday = birthday;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return toYears(LocalDate.now().toEpochDay() - birthday.toEpochDay());
+    }
+
+    private int toYears(long days) {
+        return (int) (days) / DaysPerYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthday=" + birthday +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+}
