23fabf3448b2d152bc44fc98b82e3de4ff419286
SkinnyDevi <skinnydevi.dev@gmail.com>
2024-11-24 13:45:03 +0000

    Initial commit of person and main

--- src/main/java/software/ulpgc/Main.java
+++ src/main/java/software/ulpgc/Main.java
@@ -4,16 +4,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate birthday = LocalDate.of(2002, 3, 19);
-        ContactDetails contactDetails = new ContactDetails(
-                "John Doe",
-                new ContactAddress("C/ Las Palmas", null, "35002"),
-                new PhoneNumber(34, 987654321)
-        );
-        Person johnDoe = new Person(
-                birthday,
-                contactDetails
-        );
-        System.out.println(johnDoe);
+        Person johnDoe = new Person("John Doe", 86868686, LocalDate.of(2000, 1, 1));
+        System.out.println(johnDoe  );
     }
 }
--- src/main/java/software/ulpgc/Person.java
+++ src/main/java/software/ulpgc/Person.java
@@ -4,39 +4,42 @@ import java.time.LocalDate;
 
 public class Person {
     public static final int DaysPerYear = 365;
+    private final String name;
+    private final int phoneNumber;
     private final LocalDate birthday;
-    private final ContactDetails contactDetails;
 
-    public Person(LocalDate birthday, ContactDetails contactDetails) {
+    public Person(String name, int phoneNumber, LocalDate birthday) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
         this.birthday = birthday;
-        this.contactDetails = contactDetails;
     }
 
     public int getAge() {
         return toYears(LocalDate.now().toEpochDay() - birthday.toEpochDay());
     }
 
-    private int toYears(long days) {
-        return (int) (days) / DaysPerYear;
-    }
-
     @Override
     public String toString() {
         return "Person{" +
-                "birthday=" + birthday +
-                ", contactDetails=" + contactDetails +
+                "name='" + name + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", birthday=" + birthday +
                 '}';
     }
 
+    private int toYears(long days) {
+        return (int) days / DaysPerYear;
+    }
+
     public String getName() {
-        return contactDetails.getName();
+        return name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
     }
 
     public LocalDate getBirthday() {
         return birthday;
-    }
-
-    public PhoneNumber getPhoneNumber() {
-        return contactDetails.getPhoneNumber();
     }
 }
